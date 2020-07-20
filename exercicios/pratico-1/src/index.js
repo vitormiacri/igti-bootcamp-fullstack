d0ffd7685ccca6612b40eb07d460aaf2b36594e2
Vitor Morelli <vitao.miacri@gmail.com>
2020-07-20 10:13:17 -0300

    [refact] Alterado eventListener para 'input'

--- exercicios/pratico-1/src/index.js
+++ exercicios/pratico-1/src/index.js
@@ -1,13 +1,13 @@
-var inputRed = document.querySelector("#input-red");
-var rangeRed = document.querySelector("#range-red");
+var inputRed = document.querySelector('#input-red');
+var rangeRed = document.querySelector('#range-red');
 
-var inputGreen = document.querySelector("#input-green");
-var rangeGreen = document.querySelector("#range-green");
+var inputGreen = document.querySelector('#input-green');
+var rangeGreen = document.querySelector('#range-green');
 
-var inputBlue = document.querySelector("#input-blue");
-var rangeBlue = document.querySelector("#range-blue");
+var inputBlue = document.querySelector('#input-blue');
+var rangeBlue = document.querySelector('#range-blue');
 
-var colorBox = document.querySelector("#colors");
+var colorBox = document.querySelector('#colors');
 
 const intializeValues = () => {
   inputRed.value = 0;
@@ -18,33 +18,34 @@ const intializeValues = () => {
 
   inputBlue.value = 0;
   rangeBlue.value = 0;
-}
+};
 
 const updateRGBColor = () => {
-  console.log(`rgb(${inputRed.value}, ${inputGreen.value}, ${inputBlue.value});`);
+  console.log(
+    `rgb(${inputRed.value}, ${inputGreen.value}, ${inputBlue.value});`
+  );
   colorBox.style.backgroundColor = `rgb(${inputRed.value}, ${inputGreen.value}, ${inputBlue.value})`;
-}
+};
 
 const updateRedValue = (element) => {
   inputRed.value = event.target.value;
   updateRGBColor();
-}
+};
 
 const updateGreenValue = (element) => {
   inputGreen.value = element.target.value;
   updateRGBColor();
-}
+};
 
 const updateBlueValue = (element) => {
   inputBlue.value = element.target.value;
   updateRGBColor();
-}
+};
 
 window.onload = () => {
-
   intializeValues();
 
-  rangeRed.addEventListener('change', updateRedValue)
-  rangeGreen.addEventListener('change', updateGreenValue)
-  rangeBlue.addEventListener('change', updateBlueValue)
-}
+  rangeRed.addEventListener('input', updateRedValue);
+  rangeGreen.addEventListener('input', updateGreenValue);
+  rangeBlue.addEventListener('input', updateBlueValue);
+};
